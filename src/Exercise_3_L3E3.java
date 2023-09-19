a6f6ab3763af87f3e43447308c6939d0a95e7fce
27280024 <2728024@nescol.ac.uk>
2023-09-20 00:36:46 +0100

    Exercise 3x2

--- src/Exercise_3_L3E3.java
+++ src/Exercise_3_L3E3.java
@@ -18,33 +18,30 @@ public class Exercise_3_L3E3 {
     // static so we can execute the main methods
     // void because it just runs (does not return anything
     public static void main(String[] args) {
-        System.out.print("Enter your surname");
-        // when you have a bug in IntelliJ click on the error (move cursor) and alt+enter and it will help
-//        Scanner input = new Scanner(System.in);
 
-//        String surname = input.nextLine();
-//
-//        System.out.print("Enter your year of birth");
-//        String yearofbirth = input.nextLine();
-//
-//        System.out.print("Username");
-//
-//        //System.out.print(firstname.charAt(0)); // reason it goes red is because charAt returns a character data type, not a string
-//        System.out.print(firstname.substring(0, 1).toUpperCase());
-//        //System.out.print(firstname.toUpperCase());
-//        System.out.println(surname.toLowerCase());
-//
-//        System.out.print("Password");
-//
-//        String Password = (surname.substring(0, 1));
-//        input = new Scanner(System.in);
-//
-//        System.out.print("Enter your first name");
-//        String firstname = input.nextLine();
-//        System.out.println(Password.toLowerCase());
-//        //System
-//        System.out.print(firstname.toUpperCase());
-//        System.out.print(yearofbirth);
+        // when you have a bug in IntelliJ click on the error (move cursor) and alt+enter and it will help
+       Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your firstname");
+        String firstname = input.nextLine();
+        System.out.print("Enter your surname");
+        String surname = input.nextLine();
+        System.out.print("Enter your year of birth");
+        String yearofbirth = input.nextLine();
+
+        System.out.println("Username");
+
+        //System.out.print(firstname.charAt(0)); // reason it goes red is because charAt returns a character data type, not a string
+        System.out.print(firstname.substring(0, 1).toUpperCase());
+        System.out.print(firstname.toUpperCase());
+        System.out.println(surname.toLowerCase());
+
+        System.out.println("Password");
+        System.out.print(surname.substring(0).toLowerCase());
+        System.out.print(firstname.toUpperCase());
+        System.out.print(yearofbirth);
+
+
     }
 
 
