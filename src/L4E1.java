f73d97a7d26b12cb834de01001a9dba3839f70ef
27280024 <27280024@nescol.ac.uk>
2023-09-27 11:31:51 +0100

    Merge remote-tracking branch 'origin/master'

--- src/L4E1.java
+++ src/L4E1.java
@@ -48,24 +48,7 @@ public class L4E1 {
         //
         //	System.out.format("Total = £%.2f%n", (price * num));
 
-//System.out.println("Enter the name of item 1");
-//String item1name = Scanner.nextLine();
-//System.out.println("Enter the name of item 2");
-//String item2name = Scanner.nextLine();
-//System.out.println("Enter the name of item 3");
-//String item3name = Scanner.nextLine();
-//
-//System.out.println("Enter the price of item 1");
-//int item1price = Scanner.nextDouble();
-//System.out.println("Enter the price of item 2");
-//int item2price = Scanner.nextDouble();
-//System.out.println("Enter the price of item 3");
-//int item3price = Scanner.nextDouble();
-//
-//System.out.format("%s ......... £%.2f %n", item1name, item1price);
-//System.out.format("%s ......... £%.2f %n", item2name, item2price);
-//System.out.format("%s ......... £%.2f %n", item3name, item3price);
-//System.out.format("TOTAL ... £%.2f %n", (item1price+item2price+item3price) );
+
 
 
 
