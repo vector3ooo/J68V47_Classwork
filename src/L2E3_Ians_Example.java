b6dfd000f9d15628994dd58f811a84c6e4e8c553
27280024 <2728024@nescol.ac.uk>
2023-10-02 22:36:00 +0100

    Sprawdzilismy do zadania L2E3
    Sprawdzic co jest nie tak jeszcze i dojechac potem L3 i L4

---
+++ src/L2E3_Ians_Example.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class L2E3_Ians_Example {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name of item 1");
+        String item1name = input.nextLine();
+        System.out.println("Enter the name of item 2");
+        String item2name = input.nextLine();
+        System.out.println("Enter the name of item 3");
+        String item3name = input.nextLine();
+
+        System.out.println("Enter the price of item 1");
+        double item1price = input.nextDouble();
+        System.out.println("Enter the price of item 2");
+        double item2price = input.nextDouble();
+        System.out.println("Enter the price of item 3");
+        double item3price = input.nextDouble();
+
+        System.out.format("%s ......... £%.2f %n", item1name, item1price);
+        System.out.format("%s ......... £%.2f %n", item2name, item2price);
+        System.out.format("%s ......... £%.2f %n", item3name, item3price);
+        System.out.format("TOTAL ... £%.2f %n", (item1price + item2price + item3price));
+    }
+}
