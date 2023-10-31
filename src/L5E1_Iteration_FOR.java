125301bc263d521dc14e09c934dc9fa540346696
27280024 <2728024@nescol.ac.uk>
2023-10-31 13:44:25 +0000

    Lekcja L4 Completed

--- src/L5E1_Iteration_FOR.java
+++ src/L5E1_Iteration_FOR.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 public class L5E1_Iteration_FOR {
     public static void main(string[] args){
-        System.out.println("Control Statement");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a base number");
+        int countX = input.nextInt();
 
-
-        for( int count=0; count<20; count++ )
+        for( int  count=1; count<=12; count++ )
         {
-            System.out.println(count);
+            System.out.format("%d x %d = %d %n",countX, count, (countX*count)  );
             // code to be repeated
         }
+
 
 
 
---
+++ src/L5E2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class L5E2 {
+    public static void main(string[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a base number");
+        int countX = input.nextInt();
+
+        for( int  count=1; count>=0; count++ )
+        {
+            System.out.format("%d",countX);
+
+    };    };}
