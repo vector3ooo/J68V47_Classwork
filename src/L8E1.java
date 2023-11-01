ffb0f91a3edb638c514e0aad782da7cc6090315b
27280024 <27280024@nescol.ac.uk>
2023-11-01 12:03:40 +0000

    package for lesson 2

---
+++ src/L8E1.java
@@ -0,0 +1,15 @@
+public class L8E1 {
+    public void main(string[] args){
+        String[] names = {"Ryan", "Paul", "Gracjan", "Marcus"};
+        for (int index = 0; index < names.length; index++) {
+            System.out.println( names[index] );
+
+
+        }
+        int[] scores = {78, 85, 62, 49, 98};
+        int sum = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
+
+        System.out.println("The score is: %d");
+    }
+
+}
