c564bc772757aa527dee8bde4b6144c74f6cc3b1
27280024 <2728024@nescol.ac.uk>
2023-11-08 03:27:47 +0000

    Lekcja L5 In progress

--- src/Exercice3x2.java
+++
@@ -1,6 +0,0 @@
-public class Exercice3x2 {
-    public static void main(String[] args) {
-
-    }
-
-}
--- src/L5E3.java
+++ src/L5E3.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+//
+//public class L5E3 {
+//
+//        public static void main(String[] args) {
+//            Scanner input = new Scanner(System.in);
+//
+//
+//            int score = 0;
+//            int userAnswer;
+//            int answer;
+//            for(int count=1; count <= 10; count++){
+//                System.out.format("What is %d + %d?", count, count*count);
+//                userAnswer = input.nextInt();
+//                answer = count + (count*count);
+//                if (answer == userAnswer){
+//                    System.out.println("CORRECT!");
+//                    score++;
+//                }else {
+//                    System.out.format("WRONG! The answer is %d %n");
+//                }
+//            }
+//}         System.out.format("GAME OVER. Your score is %d",score);}
 
-public class L5E3 {
 
-        public static void main(string[] args) {
-            Scanner input = new Scanner(System.in);
-
-            int score = 0;
-            int userAnswer;
-            int answer;
-            for(int count=1; count <= 10; count++){
-                System.out.format("What is %d + %d?", count, count*count);
-                userAnswer = input.nextInt();
-                answer = count + (count*count);
-                if (answer == userAnswer){
-                    System.out.println("CORRECT!");
-                    score++;
-                }else {
-                    System.out.format("WRONG! The answer is %d %n");
-                }
-            }
-}         System.out.format("GAME OVER. Your score is %d",score);}
