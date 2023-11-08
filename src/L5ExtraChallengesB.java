0625f66a3be61173ff0aadbd8558675aec1061ad
27280024 <27280024@nescol.ac.uk>
2023-11-08 16:09:18 +0000

    L5 Challenge B

--- src/L5ExtraChallengesB.java
+++ src/L5ExtraChallengesB.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 public class L5ExtraChallengesB {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int score = 0;
+        int userAnswer;
+        int answer;
+        for(int count=1; count <= 10; count++){
+            System.out.format("What is %d + %d?", count, count*count);
+            userAnswer = input.nextInt();
+            answer = count + (count*count);
+
+            if (answer == userAnswer){
+                System.out.println("CORRECT!");
+                score++;
+            }else {
+                System.out.format("WRONG! The answer is %d %n",answer);
+            }
+        }System.out.format("GAME OVER. Your score is %d",score);
 
 }}
