ce2d3e3ec4eb4170b4ee915579e1ae315cc31abc
27280024 <2728024@nescol.ac.uk>
2023-11-12 21:05:43 +0000

    Lekcja L5 In progress

--- src/Experiments_Only.java
+++ src/Experiments_Only.java
@@ -1,26 +1,18 @@
 import java.util.Scanner;
 
 public class Experiments_Only {
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
 
         //This section is to test various methology of writing code as well making understanding how it works
         Scanner input = new Scanner(System.in);
-        System.out.println("hello");
-        int age = 20;
-        int oldage = 40;
 
-        int perimeter = 2*age + 2*oldage;
+        System.out.println("Loan you want to receive");
+        float l = input.nextFloat();
+        System.out.println("Interest rate 5%");
+        float j = input.nextFloat();
+        System.out.println("Number of years for the loan");
+        int n = input.nextInt();
 
-        perimeter = 2*(age+oldage);
-
-
-        System.out.println(perimeter);
-
-
-
-
-
-
-
-
-    }}
+        //float m = (l(j/(1-(1+j)^=n)));
+        }
+    }
