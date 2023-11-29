a221b08aad8483f44b6986a267fcc117bae6af80
27280024 <27280024@nescol.ac.uk>
2023-11-29 15:57:27 +0000

    L5 Challenge B

--- src/L7E1.java
+++ src/L7E1.java
@@ -1,30 +1,46 @@
+import java.util.Scanner;
 public class L7E1 {
-    public static void printMessage(String Message){
-    System.out.println("Hello world"); // display “Hello world”
-}
     public static void main(String[] args) {
- //      printMessage("Hello World"); // call the printMessage subroutine
 
- //      {SET option variable to 0;
- //          DO
- //          {CALL showMenu;
- //      	     SET (option) TO VALUE RETURNED FROM CALL getOption;
- //      	     IF (option = 1) THEN {
- //          ;
- //          CALL option1;
- //      }ELSE {
- //              IF option = 2 THEN;
- //              CALL option2;
- //              ELSE IF option = 3 THEN;
- //              CALL option3;
- //              ELSE IF (option = 4)
- //              THEN;
- //              DISPLAY("Bye!");
- //              ELSE;
- //              DISPLAY "This isn't a valid option. Try a number between 1 and 4.”;
- //              END IF;
- //          }WHILE (option != 4);
- //      }
+        Scanner input = new Scanner(System.in);
+
+        int option = 0;
+        static void showMenu() {
+            System.out.println("Menu");
+            System.out.println("1. Option 1");
+            System.out.println("2. Option 2");
+            System.out.println("3. Option 3");
+            System.out.println("4. Exit");
+        }
+
+        static int getOption() {
+                System.out.println("Enter your choice: ");
+                return input.nextInt();
+
+        }
+    }
+
+
+       {int option = 0;
+           DO
+           {CALL showMenu;
+       	     SET (option) TO VALUE RETURNED FROM CALL getOption;
+       	     IF (option = 1) THEN {
+           ;
+           CALL option1;
+       }ELSE {
+               IF option = 2 THEN;
+               CALL option2;
+               ELSE IF option = 3 THEN;
+               CALL option3;
+               ELSE IF (option = 4)
+               THEN;
+               DISPLAY("Bye!");
+               ELSE;
+               DISPLAY "This isn't a valid option. Try a number between 1 and 4.”;
+               END IF;
+           }WHILE (option != 4);
+       }
 
 
 
